649afb11c88fb2b6bb66e0c7e561400857c69b27
WolfgangSiegert <52932241+WolfgangSiegert@users.noreply.github.com>
2020-02-14 14:58:25 +0100

    version 0.2 part2
    see version 0.2

--- Child.hpp
+++ Child.hpp
@@ -13,7 +13,8 @@ class Child : public Person
 {   
     using Person::Person;
     public:
-            /**
+         /**
+		 * DEPRECATED
          * Errechnet aus Person::qtyPerBevs und den Parametern das insgesamt konsumierte Alkoholvolumen und speichert dieses in Person::totalAlcConsumed
          * Gibt konsumierte Getraenke mit Mengen auf der Konsole aus und
          * Berechnet als Rueckgabewert den Blutalkoholwert aus Person::getBodyWeight(), Person::totalAlcConsumed und jeweiligem Koeffizienten der Unterklasse Female,Male oder Child
@@ -22,6 +23,19 @@ class Child : public Person
          * @param paramBevs vector<string> Vektor, der Namen der verfuegbaren Getraenke enthaelt -->Werte bei Index synchron zu paramBevsAlc
          * @param paramBevsAlc vector<double> Vektor, der Alkoholvolumina der verfuegbaren Getraenke enthaelt -->Werte bei Index synchron zu paramBvs
          * @returns double -->errechneter Blutalkoholwert insgesamt fuer Person und durch Person::consumedBevs eingegbene Mengen 
+         *
+		 *double calculateBloodAlc(std::vector<std::string> paramBevs,std::vector<double> paramBevsAlc) override;
+		 * 
+		 * DEPRECATED
          */
-        double calculateBloodAlc(std::vector<std::string> paramBevs,std::vector<double> paramBevsAlc) override;
+        
+		/**
+		 * Berechnet als Rueckgabewert den Blutalkoholwert aus Person::getBodyWeight(), Person::calculateTotalAlcConsumed und jeweiligem Koeffizienten der Unterklasse Female,Male oder Child
+		 * 
+		 * @pre Person::consumedBevs() wurde ausgefuehrt fuer @param
+		 * @pre Person::calculateTotalAlcConsumed() wurde ausgeführt fuer @param
+		 * @param paramTotalAlcConsumed double, gesamtes konsumiertes Alkoholvolumen
+		 * @returns double -->errechneter Blutalkoholwert insgesamt fuer Person und durch Person::consumedBevs eingegbene Mengen 
+		 */        
+        double calculateBloodAlcShort(double paramTotalAlcConsumed) override;
 };
